904260eaa535e99137fdfde4465d95163ca177f5
2ufkpfb9daxnik <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2024-11-20 22:32:08 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// console.log("■ prac00.tsの実行");
+// import "./prac00";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("■ prac01.tsの実行");
+require("./prac01");
